452c96913dd373bc7514bee2482cb96dd3a522be
Matt Toigo <mtoigo@mtoigo-dc-06219.ad.hugeinc.com>
2014-12-22 15:17:22 -0500

    PSR fixes

--- tests/KoansTest.php
+++ tests/KoansTest.php
@@ -8,12 +8,13 @@ class KoansTest
     // TODO - refactor to pull number of public methods on our $chapter
     protected $totalExercises = 0;
 
-    protected function console($message = null) {
+    protected function console($message = null)
+    {
         echo($message."\n");
     }
 
-    protected function finish() {
-
+    protected function finish()
+    {
         $percentComplete = ($this->exercisesCompleted / $this->totalExercises) * 100;
         $this->console();
         $this->console('Chapter 1 is '.$percentComplete.'%');
@@ -21,12 +22,14 @@ class KoansTest
         // TODO - logic not to output if we're at 100, or not...
     }
 
-    protected function completeExercise() {
+    protected function completeExercise()
+    {
 
         $this->exercisesCompleted++;
     }
 
-    public function __construct() {
+    public function __construct()
+    {
 
         // TODO - slick way to loop through all public methods and execute
         $this->finish();
