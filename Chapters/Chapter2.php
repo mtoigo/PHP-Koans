107748aaa62e953129481c979d1ad59427dd3c29
Matt Toigo <me@matt-toigo.com>
2015-01-04 13:29:37 -0500

    Multiplication example
    Missed something

--- Chapters/Chapter2.php
+++ Chapters/Chapter2.php
@@ -12,31 +12,49 @@ class Chapter2 extends Chapter
      * Exercise 2.1
      *
      * INSTRUCTIONS
-     * Assume the variables $valueOne and $valueTwo exist as positive integers. Set $variableSum to the result of adding them both together
+     * Assume the variables $valueOne and $valueTwo exist as positive integers. Set $sum to the result of adding them both together
+     * If $valueOne is 5 and $valueTwo is 6 then $sum should equal 11
      */
     public function addition($valueOne, $valueTwo)
     {
         // CODE HERE
-        $variableSum = $valueOne + $valueTwo;
+        $sum = $valueOne + $valueTwo;
 
         // DO NOT EDIT BELOW
-        return $variableSum;
+        return $sum;
     }
 
     /**
      * Exercise 2.2
      *
      * INSTRUCTIONS
-     * Assume the variables $valueOne and $valueTwo exist as positive integers. Set $variableDifference to
+     * Assume the variables $valueOne and $valueTwo exist as positive integers. Set $difference to
      * the result of subtracing $valueTwo from $valueOne.
+     * If $valueOne is 20 and $valueTwo is 6, $difference should be 14
      */
     public function subtraction($valueOne, $valueTwo)
     {
         // CODE HERE
-        $variableDifference = $valueOne - $valueTwo;
+        $difference = $valueOne - $valueTwo;
 
         // DO NOT EDIT BELOW
-        return $variableDifference;
+        return $difference;
+    }
+
+    /**
+     * Exercise 2.3
+     *
+     * INSTRUCTIONS
+     * Assume the variables $valueOne is a positive integer. Set the variable $fourTimes to $valueOne times 4.
+     * If $valueOne was 5, $fourTimes should end up being 20
+     */
+    public function multiplication($valueOne)
+    {
+        // CODE HERE
+        $fourTimes = $valueOne * 4;
+
+        // DO NOT EDIT BELOW
+        return $fourTimes;
     }
 
     // DO NOT EDIT BELOW
@@ -48,14 +66,19 @@ class Chapter2 extends Chapter
         $this->exerciseTests = [
             'addition' => function () {
                 if ($this->addition(22, 10) !== 32) {
-                    throw new \Exception('$variableSum is not equal to the sum of $valueOne and $valueTwo');
+                    throw new \Exception('$sum is not equal to the sum of $valueOne and $valueTwo');
                 }
             },
             'subtraction' => function () {
                 if ($this->subtraction(22, 10) !== 12) {
-                    throw new \Exception('$variableDifference is not equal to $valueTwo subtracted from $valueOne');
+                    throw new \Exception('$difference is not equal to $valueTwo subtracted from $valueOne');
                 }
             },
+            'multiplication' => function () {
+                if ($this->multiplication(6) !== 24) {
+                    throw new \Exception('$fourTimes is not equal to $valueOne times 4');
+                }
+            }
         ];
     }
 }
