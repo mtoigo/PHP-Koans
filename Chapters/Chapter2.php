7e50f7c305f169bca3c5a1a85ffca36b3b0fe1bb
Matt Toigo <me@matt-toigo.com>
2015-01-04 12:28:32 -0500

    Adding math chapter

---
+++ Chapters/Chapter2.php
@@ -0,0 +1,39 @@
+<?php
+namespace PHPKoans\Chapters;
+
+use PHPKoans\Tests\Chapter as Chapter;
+
+/**
+ * Edit the exercises below
+ */
+class Chapter2 extends Chapter
+{
+    /**
+     * Exercise 2.1
+     *
+     * INSTRUCTIONS
+     * Assume the variables $valueOne and $valueTwo exist as positive integers. Set $variableSum to the result of adding them both together
+     */
+    public function addition($valueOne, $valueTwo)
+    {
+        // CODE HERE
+
+        // DO NOT EDIT BELOW
+        return $variableSum;
+    }
+
+    // DO NOT EDIT BELOW
+    public function __construct()
+    {
+        $this->number = 2;
+        $this->name = 'Math';
+
+        $this->exerciseTests = [
+            'addition' => function() {
+                if ($this->addition(22, 10) !== 32) {
+                    throw new \Exception('$variableSum is not equal to the sum of $valueOne and $valueTwo');
+                }
+            }
+        ];
+    }
+}
--- koans.php
+++ koans.php
@@ -1,9 +1,6 @@
 <?php
 require 'vendor/autoload.php';
 
-// TODO 3. - Welcome if there's nothing filled out yet
-// TODO 2. - PHP Check for version
-
 $tests = new PHPKoans\Tests\AllTests();
 $tests->outputHeader();
 $tests->testChapters();
