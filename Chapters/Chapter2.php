de0a15a58ce490d21ebc508fb984634f614673f9
Matt Toigo <me@matt-toigo.com>
2015-01-18 23:44:30 -0500

    Removing solution

--- Chapters/Chapter2.php
+++ Chapters/Chapter2.php
@@ -63,7 +63,6 @@ class Chapter2 extends Chapter
     public function division($valueOne)
     {
         // CODE HERE
-        $oneFourth = $valueOne / 4;
 
         // DO NOT EDIT BELOW
         return $oneFourth;
