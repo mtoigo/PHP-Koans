9b618ee7d8d9376c5ce20c8c033100c09ca59085
Matt Toigo <me@matt-toigo.com>
2015-12-03 22:31:45 -0500

    Building out chapter 3 examples

--- Chapters/Chapter3.php
+++ Chapters/Chapter3.php
@@ -92,26 +92,40 @@ class Chapter3 extends Chapter
      * Exercise 3.6
      *
      * INSTRUCTIONS
+     * Assume $valueOne is a string of unknown length. Define 3 variables
+     * $partOne = first 3 characters of $valueOne
+     * $partTwo = starting at the 5th character of $valueOne and the four characters after. Total of 5 characters
+     * $partThree = last 2 characters of $valueOne
      */
-    public function substrings($valueOne, $valueTwo)
+    public function substrings($valueOne)
     {
         // CODE HERE
+        $partOne = substr($valueOne, 0, 3);
+        $partTwo = substr($valueOne, 4, 5);
+        $partThree = substr($valueOne, -2, 2);
 
         // DO NOT EDIT BELOW
-
+        return [$partOne, $partTwo, $partThree];
     }
 
     /**
      * Exercise 3.7
      *
      * INSTRUCTIONS
+     * Assume $valueOne is a string. Define 3 variables
+     * $allLower = $valueOne, but all characters are lower case
+     * $allUpper = $valueOne, but all characters are upper case
+     * $firstUpper = $valueOne, but only the first character is upper converted to upper case
      */
-    public function changingCase($valueOne, $valueTwo)
+    public function changingCase($valueOne)
     {
         // CODE HERE
+        $allLower = strtolower($valueOne);
+        $allUpper = strtoupper($valueOne);
+        $firstUpper = ucfirst($valueOne);
 
         // DO NOT EDIT BELOW
-
+        return [$allLower, $allUpper, $firstUpper];
     }
 
     /**
@@ -119,7 +133,7 @@ class Chapter3 extends Chapter
      *
      * INSTRUCTIONS
      */
-    public function searching($valueOne, $valueTwo)
+    public function searching($valueOne)
     {
         // CODE HERE
 
@@ -157,6 +171,30 @@ class Chapter3 extends Chapter
             'singleCharacters' => function() {
                 if ($this->singleCharacters('tacos') !== 'c') {
                     throw new \Exception('$valueTwo should be equal to the 3rd character of $valueOne');
+                }
+            },
+            'substrings' => function() {
+                $result = $this->substrings('Thanks for wondering how this works.');
+                if ($result[0] !== 'Tha') {
+                    throw new \Exception('$partOne must be equal to the first 3 characters of $valueOne');
+                }
+                if ($result[1] !== 'ks fo') {
+                    throw new \Exception('$partTwo must be equal to 5 characters starting at the 5th character of $valueOne');
+                }
+                if ($result[2] !== 's.') {
+                    throw new \Exception('$partThree must be equal to the last 2 characters of $valueOne');
+                }
+            },
+            'changingCase' => function() {
+                $result = $this->changingCase('sampleCase');
+                if ($result[0] !== 'samplecase') {
+                    throw new \Exception('$allLower must be equal to $valueOne with all characters lower case');
+                }
+                if ($result[1] !== 'SAMPLECASE') {
+                    throw new \Exception('$allUpper must be equal to $valueOne with all characters upper case');
+                }
+                if ($result[2] !== 'SampleCase') {
+                    throw new \Exception('$firstUpper must be equal to $valueOne with just the first character upper case');
                 }
             }
         ];
